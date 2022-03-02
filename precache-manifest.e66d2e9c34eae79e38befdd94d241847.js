5f82c212ef5b58dac448302b141d1a5ac51f9a0c
Mahmod Jnead <mjnead2001@gmail.com>
2022-03-02 13:57:25 +0200

    Updates

---
+++ precache-manifest.e66d2e9c34eae79e38befdd94d241847.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e2f193d57224e64ea2239a6dd46be1f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "28f66457dfbb7ed9dc8c",
+    "url": "/todolist/static/css/main.0b7c96a2.chunk.css"
+  },
+  {
+    "revision": "987146ab9ca97764bf68",
+    "url": "/todolist/static/js/2.28219d4a.chunk.js"
+  },
+  {
+    "revision": "97f1161acd0e6d97385ef7b190478233",
+    "url": "/todolist/static/js/2.28219d4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28f66457dfbb7ed9dc8c",
+    "url": "/todolist/static/js/main.aa14149c.chunk.js"
+  },
+  {
+    "revision": "4e52eb6eb21bd742415f",
+    "url": "/todolist/static/js/runtime-main.287a0bd4.js"
+  }
+]);
